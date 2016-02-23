314c02304182dcd26e1deb04b4046ae0e8011135
xiang zhihong <xiangzhihong8@sina.com>
2016-02-23 11:21:34 +0800

    第二次提交

---
+++ app/src/main/java/com/xzh/news/activities/WelcomeActivity.java
@@ -0,0 +1,30 @@
+package com.xzh.news.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.xzh.news.MainActivity;
+import com.xzh.news.R;
+
+/**
+ * 启动页
+ */
+public class WelcomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        //使用Handler发送一个延迟1000ms的消息
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);//设定睡眠时间1000ms
+    }
+}
